008e04496d73dec34a14aebfe6aae9e7ec367732
turboBarbitura <81514908+turboBarbitura@users.noreply.github.com>
2022-05-10 11:53:22 +0700

    NavBar Created

---
+++ components/Featured.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "../styles/Featured.module.css";
+
+const Featured = () => {
+  return <div> f </div>;
+};
+
+export default Featured;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,36 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="phone" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>+7-923-923-77-77</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="logo" width="160" height="69" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="cart" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
